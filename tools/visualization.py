a1f8ff7fddd4dbfbe571db80cfd0a5510abdea40
Laura Estacio Cerquin <cerquinl@int6.local.snellius.surf.nl>
2024-06-12 17:41:35 +0200

    Minor changes

--- tools/visualization.py
+++ tools/visualization.py
@@ -4,32 +4,7 @@ import SimpleITK as sitk
 import matplotlib.pyplot as plt
 from   tools.utils import convert_tensor_to_numpy
 
-def plot_sample_data(sample, slide=80):
-    
-    kps_i = np.zeros(sample['voxel1'][:, :, slide].shape)
-    kps_e = np.zeros(sample['voxel2'][:, :, slide].shape)
-    
-    #for kp in sample['kps_i'][sample['kps_i'][:, 2] == slide]:
-    #    kps_i[int(kp[1]), int(kp[0])] = 1
-    #for kp in sample['kps_e'][sample['kps_e'][:, 2] == slide]:
-    #    kps_e[int(kp[1]), int(kp[0])] = 1
-    
-    
-    fig, axs = plt.subplots(2,2, figsize=(10,10))
-    axs[0,0].imshow(sample['voxel1'][:, :, slide], cmap='gray')
-    axs[0,1].imshow(sample['voxel2'][:,:, slide], cmap='gray')
-    axs[1,0].imshow(sample['segmentation1'][:,:, slide], cmap='gray')
-    axs[1,1].imshow(sample['segmentation2'][:,:, slide], cmap='gray')
-    #axs[2,0].imshow(kps_i, cmap='gray')
-    #axs[2,1].imshow(kps_e, cmap='gray')
-    axs[0, 0].axis('off')
-    axs[0, 1].axis('off')
-    axs[1, 0].axis('off')
-    axs[1, 1].axis('off')
-    #axs[2, 0].axis('off')
-    #axs[2, 1].axis('off')
-    plt.tight_layout()
-    plt.show()
+
 
 def plot_sample_data(sample, slide=80, save_path=None):
     
@@ -50,10 +25,10 @@ def plot_sample_data(sample, slide=80, save_path=None):
         kps_e[int(kp[1]), int(kp[0])] = 1
     
     fig, axs = plt.subplots(3, 2, figsize=(10, 10))
-    axs[0, 0].imshow(voxel1[slide, :, :], cmap='gray')
-    axs[0, 1].imshow(voxel2[slide, :, :], cmap='gray')
-    axs[1, 0].imshow(segmentation1[slide, :, :], cmap='gray')
-    axs[1, 1].imshow(segmentation2[slide, :, :], cmap='gray')
+    axs[0, 0].imshow(voxel1[:, :, slide], cmap='gray') # slide shouls be the last axis
+    axs[0, 1].imshow(voxel2[:, :, slide], cmap='gray')
+    axs[1, 0].imshow(segmentation1[:, :, slide], cmap='gray')
+    axs[1, 1].imshow(segmentation2[:, :, slide], cmap='gray')
     # Uncomment if you need to visualize keypoints
     axs[2, 0].imshow(kps_i, cmap='gray')
     axs[2, 1].imshow(kps_e, cmap='gray')
