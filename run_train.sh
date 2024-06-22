81c1bd037a4256008b91ccb61644b70de60c7e27
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2024-06-22 22:50:32 +0200

    Minor changes sbatch file

--- run_train.sh
+++ run_train.sh
@@ -2,8 +2,8 @@
 #SBATCH --time=1-00:00:00                        # Time limit hrs:min:sec
 #SBATCH --job-name=lung_vxm                    # Job name
 #SBATCH --qos=a6000_qos
-#SBATCH --partition=rtx8000                        # Partition
-#SBATCH --nodelist=roentgen                    # Node name
+#SBATCH --partition=a6000                        # Partition
+#SBATCH --nodelist=galileo                    # Node name
 #SBATCH --nodes=1
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=10
@@ -13,6 +13,7 @@ pwd; hostname; date
 
 # Activate conda environment pyenv
 source /home/l.estacio/miniconda3/bin/activate pytorch
+rsync -avv --info=progress2 --ignore-existing /data/groups/beets-tan/l.estacio/lung_data/LungCT /processing/l.estacio/
 
 # Run your command
 python /projects/disentanglement_methods/Med_Align_Net/train.py
