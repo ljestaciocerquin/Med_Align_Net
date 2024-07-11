71efec027ee8b62c64ad39502052c498e7060d90
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2024-07-11 12:10:35 +0200

    Correcting axis of the images

--- eval/_elastix.py
+++ eval/_elastix.py
@@ -1,34 +1,5 @@
-import ants
 import numpy as np
 import SimpleITK as sitk 
-
-def ants_pred(fixed, moving, seg2):
-    """return warped and w_seg2 """
-    warps   = []
-    w_seg2s = []
-    flows   = []
-    
-    for i in range(fixed.shape[0]):
-        im_fixed  = ants.from_numpy(fixed.cpu().numpy()[i,0])
-        im_moving = ants.from_numpy(moving.cpu().numpy()[i,0])
-        reg       = ants.registration(fixed=im_fixed, moving=im_moving, type_of_transform='SyN')
-        warped    = ants.apply_transforms(fixed=im_fixed, moving=im_moving, transformlist=reg['fwdtransforms'])
-        warps.append(warped.numpy())
-        w_seg2    = ants.apply_transforms(fixed=im_fixed, moving=ants.from_numpy(seg2.cpu().numpy()[i,0]), transformlist=reg['fwdtransforms'])
-        w_seg2s.append(w_seg2.numpy())
-        flow      = ants.image_read(reg['fwdtransforms'][0])
-        flows.append(flow.numpy())
-    
-    w_seg2 = np.array(w_seg2s)[:, None]
-    warped = np.array(warps)[:, None]
-    flow   = np.transpose(np.array(flows), (0, 4, 1, 2, 3))
-    
-    return {
-        "warped": warped,
-        "w_seg2": w_seg2,
-        "flow":   flow,
-    }
-
 
 def elastix_pred(fixed, moving, seg2):
     """return warped, flow, and w_seg2 """
@@ -38,12 +9,7 @@ def elastix_pred(fixed, moving, seg2):
     
     for i in range(fixed.shape[0]):
         # Convert images to SimpleITK format
-        
-        print('T Fixed: ',  fixed.shape)
-        print('T Moving: ', moving.shape)
-        print('T Segm: ',   seg2.shape)
-        
-        im_fixed  = sitk.GetImageFromArray(fixed.cpu().numpy()[i, 0])
+        im_fixed  = sitk.GetImageFromArray(fixed.cpu().numpy()[i, 0]) # 1 x 1 x 192 x 192 x 208 -----> 192 x 192 x 208 -----> 208 x 192 x 192
         im_moving = sitk.GetImageFromArray(moving.cpu().numpy()[i, 0])
         im_seg2   = sitk.GetImageFromArray(seg2.cpu().numpy()[i, 0])
         
@@ -64,24 +30,26 @@ def elastix_pred(fixed, moving, seg2):
         elastixImageFilter.Execute()
         
         # Get the result image (warped image)
-        result_image = elastixImageFilter.GetResultImage()
-        warps.append(np.moveaxis(sitk.GetArrayFromImage(result_image), 0, -1))
-
+        warped = elastixImageFilter.GetResultImage()
+        warps.append(sitk.GetArrayFromImage(warped)) # 208 x 192 x 192 -----> 192 x 192 x 208  
+        
         # Apply the same transformation to the segmentation map
         transformixImageFilter = sitk.TransformixImageFilter()
-        transformixImageFilter.SetMovingImage(im_seg2)
         transformixImageFilter.SetTransformParameterMap(elastixImageFilter.GetTransformParameterMap())
+        transformixImageFilter.SetMovingImage(im_seg2)
         transformixImageFilter.Execute()
         w_seg2_image = transformixImageFilter.GetResultImage()
-        w_seg2s.append( np.moveaxis(sitk.GetArrayFromImage(w_seg2_image), 0, -1))
-
+        w_seg2s.append(sitk.GetArrayFromImage(w_seg2_image))
+        #import pdb; pdb.set_trace()
+        
         # Get the deformation field
-        transformParameterMap = elastixImageFilter.GetTransformParameterMap()
-        transformixImageFilter.SetTransformParameterMap(transformParameterMap)
-        transformixImageFilter.ComputeDeformationFieldOn()
-        transformixImageFilter.Execute()
-        flow = transformixImageFilter.GetDeformationField()
-        flows.append(flow)#(np.moveaxis(sitk.GetArrayFromImage(flow), 0, -1))
+        transform = elastixImageFilter.GetTransformParameterMap()[0]
+        displacement_field = sitk.TransformToDisplacementField(sitk.Transform(transform), 
+                                                               referenceImage=im_fixed, 
+                                                               outputPixelType=sitk.sitkVectorFloat64)
+        displacement_field_array = sitk.GetArrayFromImage(displacement_field)
+        flows.append(displacement_field_array)
+        #import pdb; pdb.set_trace()
     
     # Convert results to numpy arrays
     w_seg2 = np.array(w_seg2s)[:, None]
