62775c80c7020d6eaae46a11cc3a3a74ac3a86c1
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2024-07-30 21:37:20 +0200

    Adding dataset Abdomen I

--- data_util/dataset.py
+++ data_util/dataset.py
@@ -1,6 +1,7 @@
 #%%
 import json
 import torch
+import random
 import pandas as pd
 from torch.utils.data import Dataset
 import sys
@@ -9,8 +10,9 @@ from processing.cts_processors import ScanProcessor
 from processing.cts_operations import ReadVolume
 from processing.cts_operations import ToNumpyArray
 from processing.cts_operations import ToLungWindowLevelNormalization
+from processing.cts_operations import ToAbdomenWindowLevelNormalization
 
-class RawData():
+class RawDataLung():
     def __init__(self, json_file, root_dir, mode='train', transform=None):
         with open(json_file, 'r') as file:
             data_info = json.load(file)
@@ -132,31 +134,186 @@ class RawData():
             ret['lmks1'] = lmks1
             ret['lmks2'] = lmks2
 
+        print('ret[voxel1]: ', ret['voxel1'].shape)
+        print('ret[segmentation2]: ', ret['segmentation2'].shape)
+        
         return ret
             
+
+
+class RawDataAbdomen():
+    def __init__(self, json_file, root_dir, mode='train', transform=None):
+        with open(json_file, 'r') as file:
+            data_info = json.load(file)
+        self.root_dir  = root_dir
+        self.mode      = mode
+        self.transform = transform
+        self.inp_dtype = torch.float32
+        self.scan_loader  = self.__init_scan_loader()
+        self.label_loader = self.__init_label_loader()
         
- 
+        # We didn't consider registration_val since they are the three first elements of the training dataset
+        # This 3 elements have landmarks information
+        mode_mapping = {
+            'train': 'training',
+            'val':   'registration_val',
+            'test' : 'registration_test'
+        }
+        
+        if self.mode not in mode_mapping:
+            raise ValueError('mode can only be train, val, or test')
+        
+        data      = data_info[mode_mapping[self.mode]]
+        
+        if self.mode == 'train':   
+            data_temp = data_info[mode_mapping['val']]
+            self.data = self.get_pairs_with_gt_train(data, data_temp)
+        else:
+            self.data = self.get_pairs_with_gt_test(data)
+        self.add_root_dir_to_paths()
+    
+    
+    
+    def get_pairs_with_gt_train(self, data, data_temp):
+        val_list  = sorted(list(set( [x['fixed'] for x in data_temp] + [x['moving'] for x in data_temp] )))
+        training_ = [x for x in data if x['image'] not in val_list]
+        pairs     = []
+        
+        for entry in training_:
+            fixed_image  = entry['image']
+            moving_image = random.choice(training_)['image']
+            fixed_mask   = fixed_image.replace('imagesTr', 'labelsTr')
+            moving_mask  = moving_image.replace('imagesTr', 'labelsTr')
+            pair = {
+                'fix': fixed_image,
+                'mov': moving_image,
+                'fix_seg': fixed_mask,
+                'mov_seg': moving_mask
+            }
+            pairs.append(pair)
+        return pairs
+    
+    
+    def get_pairs_with_gt_test(self, data):
+        pairs     = []
+        for entry in data:
+            fixed_image  = entry['fixed']
+            moving_image = entry['moving']
+            fixed_mask   = fixed_image.replace('imagesTr', 'labelsTr')
+            moving_mask  = moving_image.replace('imagesTr', 'labelsTr')
+            
+            pair = {
+                'fix': fixed_image,
+                'mov': moving_image,
+                'fix_seg': fixed_mask,
+                'mov_seg': moving_mask
+            }
+            pairs.append(pair)
+        return pairs
+    
+    
+    def add_root_dir_to_paths(self):
+        for pair in self.data:
+            for key, value in pair.items():
+                pair[key] = self.root_dir + value.lstrip('./')
+                    
+                    
+        
+    def __init_label_loader(self):
+        return ScanProcessor(
+            ReadVolume(),
+            ToNumpyArray()
+        )
+        
+              
+    def __init_scan_loader(self):
+        return ScanProcessor(
+            ReadVolume(),
+            ToAbdomenWindowLevelNormalization()
+        )
+    
+    def __len__(self):
+        return len(self.data)
+    
+    
+    
+    def __getitem__(self, idx: int):
+        
+        ret = {}
+        print('Helooooooooooooooooooo: ', self.data[idx]['fix'])
+        voxel1        = torch.from_numpy(self.scan_loader(self.data[idx]['fix'])).type(self.inp_dtype)
+        voxel2        = torch.from_numpy(self.scan_loader(self.data[idx]['mov'])).type(self.inp_dtype)
+        segmentation1 = torch.from_numpy(self.label_loader(self.data[idx]['fix_seg'])).type(self.inp_dtype)
+        segmentation2 = torch.from_numpy(self.label_loader(self.data[idx]['mov_seg'])).type(self.inp_dtype)
+        
+        ret['img1_path']     = self.data[idx]['fix']
+        ret['img2_path']     = self.data[idx]['mov']
+        ret['voxel1']        = voxel1[None, :]
+        ret['voxel2']        = voxel2[None, :]
+        ret['segmentation1'] = segmentation1[None, :]
+        ret['segmentation2'] = segmentation2[None, :]
+
+        return ret
+
+
+
+class RawData():  
+    def __init__(self, args, **kwargs):    
+        data_type      = 'abdomen' if 'abdomen' in args else 'lung'
+        if data_type == 'lung':
+            self.raw_data = RawDataLung(args, **kwargs)
+        else:
+            self.raw_data = RawDataAbdomen(args, **kwargs)
+    
+    def __len__(self):
+        return len(self.raw_data)
+    
+    def __getitem__(self, idx):
+        return self.raw_data[idx]
+         
 class Data(RawData, Dataset):
     def __init__(self, args, **kwargs):
-        RawData.__init__(self, args, **kwargs)
+        super().__init__(args, **kwargs)
+        #print('data - args: '. args)
+        #print('data - kwargs: ', kwargs)
         
-
 
 #%%
 import sys 
 sys.path.append('..')
 from tools.utils         import show_img
 from tools.visualization import plot_sample_data
+from tools.visualization import plot_sample_data_and_kpts
 
 import matplotlib.pyplot as plt
 if __name__  == '__main__':
-    data_file = '/data/groups/beets-tan/l.estacio/lung_data/LungCT/LungCT_dataset.json'
+    # Lung dataset:
+    '''data_file = '/data/groups/beets-tan/l.estacio/lung_data/LungCT/LungCT_dataset.json'
     root_dir  = '/processing/l.estacio/LungCT/'
-
-
+    data      = Data(data_file, root_dir=root_dir, mode='val')
+    plot_sample_data_and_kpts(data[0], slide=128, save_path='./128_.png')'''
+    
+    # Abdomen
+    data_file = '/data/groups/beets-tan/l.estacio/abdomen_data/AbdomenCTCT/AbdomenCTCT_dataset.json'
+    root_dir  = '/processing/l.estacio/AbdomenCTCT/'
     data      = Data(data_file, root_dir=root_dir, mode='val')
     print(len(data))
-    #plot_sample_data(data[0], slide=128, save_path='./128_.png')
-    print(data[0])
+    plot_sample_data(data[0], slide=128, save_path='./128_.png')
+    #print(data[0])
 
+# %%
+#import os
+#import json
+#data_path='/data/groups/beets-tan/l.estacio/abdomen_data/AbdomenCTCT/'
+#with open(os.path.join(data_path,'AbdomenCTCT_dataset.json')) as f:
+#    dataset_info=json.load(f)
+
+#val_list=sorted(list(set([x['fixed'] for x in dataset_info['registration_val']] 
+#             + [x['moving'] for x in dataset_info['registration_val']])))
+#validation_ = dataset_info['registration_val']
+#training_ = [x for x in dataset_info['training'] if x['image'] not in val_list]
+#print(training_)
+#H,W,D = dataset_info['tensorImageShape']['0']
+#num_val=len(val_list); num_train=len(training_)
+#print('Training:',len(training_),'; Validation',len(val_list))
 # %%
