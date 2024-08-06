86a8339940b91f8340ed6a099d18304c4edb0224
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2024-08-06 16:13:13 +0200

    Adding AligNet by considering only the concatenatition of layers in AligNet_elastic.py

--- networks/ALIGNET/AligNet_elastic.py
+++ networks/ALIGNET/AligNet_elastic.py
@@ -82,27 +82,27 @@ class AligNet(nn.Module):
 
         #self.conv_ls = convolveLeakyReLU(  in_channels*2, 32*channels, 3, 2, dim=dim)
         
-        self.pred6      = convolve(32 * channels * 2, dim, 3, 1, dim=dim)
+        self.pred6      = convolve(2 * 32 * channels, dim, 3, 1, dim=dim)
         self.upsamp6to5 = upconvolve(dim, dim, 4, 2, dim=dim)
-        self.deconv5    = upconvolveLeakyReLU(32 * channels, 16 * channels, 4, 2, dim=dim)
+        self.deconv5    = upconvolveLeakyReLU(2 * 32 * channels, 16 * channels, 4, 2, dim=dim)
 
-        self.pred5      = convolve(32 * channels + dim, dim, 3, 1, dim=dim)  # 514 = 32 * channels + 1 + 1
+        self.pred5      = convolve(3 * 16 * channels + dim, dim, 3, 1, dim=dim)  # 514 = 32 * channels + 1 + 1
         self.upsamp5to4 = upconvolve(dim, dim, 4, 2, dim=dim)
-        self.deconv4    = upconvolveLeakyReLU(32 * channels + dim, 8 * channels, 4, 2, dim=dim)
+        self.deconv4    = upconvolveLeakyReLU(3 * 16 * channels + dim, 8 * channels, 4, 2, dim=dim)
 
-        self.pred4      = convolve(16 * channels + dim, dim, 3, 1, dim=dim)  # 258 = 64 * channels + 1 + 1
+        self.pred4      = convolve(3 * 8 * channels + dim, dim, 3, 1, dim=dim)  # 258 = 64 * channels + 1 + 1
         self.upsamp4to3 = upconvolve(dim, dim, 4, 2, dim=dim)
-        self.deconv3    = upconvolveLeakyReLU(16 * channels + dim,  4 * channels, 4, 2, dim=dim)
+        self.deconv3    = upconvolveLeakyReLU(3 * 8 * channels + dim,  4 * channels, 4, 2, dim=dim)
 
-        self.pred3      = convolve(8 * channels + dim, dim, 3, 1, dim=dim)
+        self.pred3      = convolve(3 * 4 * channels + dim, dim, 3, 1, dim=dim)
         self.upsamp3to2 = upconvolve(dim, dim, 4, 2, dim=dim)
-        self.deconv2    = upconvolveLeakyReLU(8 * channels + dim, 2 * channels, 4, 2, dim=dim)
+        self.deconv2    = upconvolveLeakyReLU(3 * 4 * channels + dim, 2 * channels, 4, 2, dim=dim)
 
-        self.pred2      = convolve(4 * channels + dim, dim, 3, 1, dim=dim)
+        self.pred2      = convolve(3 * 2 * channels + dim, dim, 3, 1, dim=dim)
         self.upsamp2to1 = upconvolve(dim, dim, 4, 2, dim=dim)
-        self.deconv1    = upconvolveLeakyReLU(4 * channels + dim, channels, 4, 2, dim=dim)
+        self.deconv1    = upconvolveLeakyReLU(3 * 2 * channels + dim, channels, 4, 2, dim=dim)
 
-        self.pred0      = upconvolve(2 * channels + dim, dim, 4, 2, dim=dim)
+        self.pred0      = upconvolve(3 * channels + dim, dim, 4, 2, dim=dim)
         
     
     def forward(self, fixed, moving, return_neg = False, hyp_tensor=None):
@@ -134,38 +134,40 @@ class AligNet(nn.Module):
         y6_1 = self.conv6_1(y6)                             # 512 x 8 x 8           # 512 x   3 x   3 x   4
         
         # Concatenation of fixed and moving 
-        xy   = torch.cat((x6_1, y6_1), dim=1)
-        #import pdb; 
-        #pdb.set_trace()
+        xy   = torch.cat((x6_1, y6_1), dim=1)                                       # 1024 x   3 x   3 x   4
         pred6      = self.pred6(xy)                               # 2 x 8 x 8         #   3 x 3 x 3 x 4
-        upsamp6to5 = self.upsamp6to5(pred6)                         # 2 x 16 x 16       #   3 x 6 x 6 x 8
-        deconv5    = self.deconv5(x6_1)                             # 256 x 16 x 16     # 256 x 6 x 6 x 8
+        upsamp6to5 = self.upsamp6to5(pred6)                         # 2 x 16 x 16     #   3 x 6 x 6 x 8
+        deconv5    = self.deconv5(xy)                             # 256 x 16 x 16     # 256 x 6 x 6 x 8
         # Funtion to get the same size in dimension 4 
         # in order to be able to concat the tensors. 
-        tensors    = get_same_dim_tensors([x5_1, deconv5, upsamp6to5], x5_1.size(-1), -1)
-        concat5    = torch.cat(tensors, dim=1)                      # 514 x 16 x 16     # 515 x 6 x 6 x 7
+        tensors    = get_same_dim_tensors([x5_1, y5_1, deconv5, upsamp6to5], x5_1.size(-1), -1)
+        concat5    = torch.cat(tensors, dim=1)                      # 514 x 16 x 16     # 771 x 6 x 6 x 7
 
-        pred5      = self.pred5(concat5)                            # 2 x 16 x 16       #   
-        upsamp5to4 = self.upsamp5to4(pred5)                         # 2 x 32 x 32       #   
-        deconv4    = self.deconv4(concat5)                          # 2 x 32 x 32       #  
-        tensors    = get_same_dim_tensors([x4_1, deconv4, upsamp5to4], x4_1.size(-1), -1)
-        concat4    = torch.cat(tensors, dim=1)                      # 258 x 32 x 32     # 
-
-        pred4      = self.pred4(concat4)                            # 2 x 32 x 32
-        upsamp4to3 = self.upsamp4to3(pred4)                         # 2 x 64 x 64
-        deconv3    = self.deconv3(concat4)                          # 64 x 64 x 64
-        concat3    = torch.cat([x3_1, deconv3, upsamp4to3], dim=1)  # 130 x 64 x 64
-
-        pred3      = self.pred3(concat3)                            # 2 x 63 x 64
-        upsamp3to2 = self.upsamp3to2(pred3)                         # 2 x 128 x 128
-        deconv2    = self.deconv2(concat3)                          # 32 x 128 x 128
-        concat2    = torch.cat([x2, deconv2, upsamp3to2], dim=1)    # 66 x 128 x 128
         
+        pred5      = self.pred5(concat5)                            # 2 x 16 x 16       #  3 x 6 x 6 x 7
+        upsamp5to4 = self.upsamp5to4(pred5)                         # 2 x 32 x 32       #  3 x 12 x 12 x 14
+        deconv4    = self.deconv4(concat5)                          # 2 x 32 x 32       #  128 x 12 x 12 x 14
+        tensors    = get_same_dim_tensors([x4_1, y4_1, deconv4, upsamp5to4], x4_1.size(-1), -1)
+        concat4    = torch.cat(tensors, dim=1)                      # 258 x 32 x 32     # 387 x 12 x 12 x 13
+
+        pred4      = self.pred4(concat4)                            # 2 x 32 x 32       # 3 x 12 x 12 x 13
+        upsamp4to3 = self.upsamp4to3(pred4)                         # 2 x 64 x 64       # 3 x 24 x 24 x 26
+        deconv3    = self.deconv3(concat4)                          # 64 x 64 x 64      # 64 x 24 x 24 x 26
+        concat3    = torch.cat([x3_1, y3_1, deconv3, upsamp4to3], dim=1)  # 130 x 64 x 64 # 195 x 24 x 24 x 26
+
         
-        pred2      = self.pred2(concat2)                            # 2 x 128 x 128
-        upsamp2to1 = self.upsamp2to1(pred2)                         # 2 x 256 x 256
-        deconv1    = self.deconv1(concat2)                          # 16 x 256 x 256
-        concat1    = torch.cat([x1, deconv1, upsamp2to1], dim=1)    # 34 x 256 x 256
+        pred3      = self.pred3(concat3)                            # 2 x 63 x 64           # 3 x 24 x 24 x 26
+        upsamp3to2 = self.upsamp3to2(pred3)                         # 2 x 128 x 128         # 3 x 48 x 48 x 52
+        deconv2    = self.deconv2(concat3)                          # 32 x 128 x 128        # 32 x 48 x 48 x 52
+        concat2    = torch.cat([x2, y2, deconv2, upsamp3to2], dim=1)    # 66 x 128 x 128    # 99 x 48 x 48 x 52
+        
+        #import pdb; 
+        #pdb.set_trace()
+        
+        pred2      = self.pred2(concat2)                            # 2 x 128 x 128         # 3 x 48 x 48 x 52
+        upsamp2to1 = self.upsamp2to1(pred2)                         # 2 x 256 x 256         # 3 x 96 x 96 x 104
+        deconv1    = self.deconv1(concat2)                          # 16 x 256 x 256        # 16 x 96 x 96 x 104
+        concat1    = torch.cat([x1, y1, deconv1, upsamp2to1], dim=1)    # 34 x 256 x 256    # 51 x 96 x 96 x 104
 
         pred0      = self.pred0(concat1)                            # 2 x 512 x 512
 
