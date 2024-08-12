ec22252643c980643a8b4271d60871d7f26f72ca
ljestaciocerquin <lj.estacio.cerquin@gmail.com>
2024-08-12 14:49:28 +0200

    minor changes in run_train.sh file

--- run_train.sh
+++ run_train.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #SBATCH --time=5-00:00:00                        # Time limit hrs:min:sec
-#SBATCH --job-name=lung                    # Job name
+#SBATCH --job-name=abd                    # Job name
 #SBATCH --qos=a6000_qos
 #SBATCH --partition=rtx8000                        # Partition
 #SBATCH --nodelist=roentgen                    # Node name
@@ -8,7 +8,7 @@
 #SBATCH --gpus-per-node=1
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=30G
-#SBATCH --output=/projects/disentanglement_methods/Med_Align_Net/logs/aln_lng_%j.log   # Standard output and error log
+#SBATCH --output=/projects/disentanglement_methods/Med_Align_Net/logs/aln_abd_1_%j.log   # Standard output and error log
 pwd; hostname; date
 
 # Activate conda environment pyenv
@@ -19,4 +19,5 @@ source /home/l.estacio/miniconda3/bin/activate pytorch
 #python /projects/disentanglement_methods/Med_Align_Net/train.py
 #python /projects/disentanglement_methods/Med_Align_Net/train.py -base VTN
 #python /projects/disentanglement_methods/Med_Align_Net/train.py -base TSM
-python /projects/disentanglement_methods/Med_Align_Net/train.py -base ALN -r 20 -d /processing/l.estacio/LungCT/LungCT_dataset.json -rd /processing/l.estacio/LungCT/ 
+python /projects/disentanglement_methods/Med_Align_Net/train.py -base ALN
+#python /projects/disentanglement_methods/Med_Align_Net/train.py -base ALN -r 20 -d /processing/l.estacio/LungCT/LungCT_dataset.json -rd /processing/l.estacio/LungCT/ 
